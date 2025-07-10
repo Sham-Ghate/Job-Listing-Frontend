a4faa36ef2e4ebfd7ee5620b89a148b3f210d213
sham.g <sham.g@pixel6.co>
2025-07-10 10:41:52 +0530

    created separate routes for admin and candidate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Admin pages
+import AddJobForm from "./pages/admin/AddJobForm";
+import EditJobForm from "./pages/admin/EditJobForm";
+import JobListAdmin from "./pages/admin/JobListAdmin";
+
+// Candidate pages
+import JobListCandidate from "./pages/candidate/JobListCandidate";
+import JobDetailPage from "./pages/candidate/JobDetailPage";
+import ApplyForm from "./pages/candidate/ApplyForm";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+
+        {/* Candidate Routes */}
+        <Route path="/" element={<JobListCandidate />} />
+        <Route path="/jobs/:id" element={<JobDetailPage />} />
+        <Route path="/jobs/:id/apply" element={<ApplyForm />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin/jobs" element={<JobListAdmin />} />
+        <Route path="/add" element={<AddJobForm />} />
+        <Route path="/edit/:id" element={<EditJobForm />} />
+
+      </Routes>
+    </Router>
   );
 }
 
